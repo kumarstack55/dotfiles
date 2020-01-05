02057067b4697af7889a9f4866304f1c8070c835
kumarstack55 <kumarstack55@gmail.com>
2020-01-05 15:52:25 +0900

    nvimあればEDITORにnvimを指定する

--- src/dot.bashrc.d/env-editor.sh
+++ src/dot.bashrc.d/env-editor.sh
@@ -1,5 +1,7 @@
 # env-editor.sh
-if type vim >/dev/null 2>&1; then
+if type nvim >/dev/null 2>&1; then
+  export EDITOR=nvim
+elif type vim >/dev/null 2>&1; then
   export EDITOR=vim
 elif type vi >/dev/null 2>&1; then
   export EDITOR=vi
