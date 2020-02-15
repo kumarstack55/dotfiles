fe8ec1882cce95ca9ff323e60a322e60d383ca91
kumarstack55 <kumarstack55@gmail.com>
2020-02-15 23:01:28 +0900

    git-bash では nvim を EDITOR にしない

--- src/dot.bashrc.d/env-editor.sh
+++ src/dot.bashrc.d/env-editor.sh
@@ -1,6 +1,10 @@
 # env-editor.sh
 if type nvim >/dev/null 2>&1; then
-  export EDITOR=nvim
+  if [[ $(uname -s) =~ ^MINGW64_NT ]]; then
+    export EDITOR=vim
+  else
+    export EDITOR=nvim
+  fi
 elif type vim >/dev/null 2>&1; then
   export EDITOR=vim
 elif type vi >/dev/null 2>&1; then
