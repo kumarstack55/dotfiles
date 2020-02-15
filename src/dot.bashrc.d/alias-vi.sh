ef7479b831211fe341e253e2b939e3cefa772c8d
kumarstack55 <kumarstack55@gmail.com>
2020-02-15 22:21:59 +0900

    git-bash では nvim があってもエイリアスを作らない

--- src/dot.bashrc.d/alias-vi.sh
+++ src/dot.bashrc.d/alias-vi.sh
@@ -1,6 +1,8 @@
 # alias-vi.sh
 if type nvim >/dev/null 2>&1; then
-  alias vi=nvim
+  if [[ ! $(uname -s) =~ ^MINGW64_NT ]]; then
+    alias vi=nvim
+  fi
 elif type vim >/dev/null 2>&1; then
   alias vi=vim
 fi
