0b04919e50a404d699e7e8b4a5e79ed24c1bb40c
kumarstack55 <kumarstack55@gmail.com>
2020-04-11 09:17:22 +0000

    テストを加えた

---
+++ tests/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -eu
+
+script_path=$(readlink -f $0)
+script_dir=$(dirname $script_path)
+for test_path in $(find "$script_dir" -type f -name "test_*.sh"); do
+  echo "test_path: $test_path"
+  $test_path
+done
