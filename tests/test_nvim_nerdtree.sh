2a9c6d0303a3f3b45c919b5663ebae80f735da5a
kumarstack55 <kumarstack55@gmail.com>
2020-04-11 10:15:54 +0000

    不要な空白を消した

--- tests/test_nvim_nerdtree.sh
+++ tests/test_nvim_nerdtree.sh
@@ -16,7 +16,6 @@ cat <<__SEQ__ | tee $seq_file
 :w ${out_file}
 :qa!
 __SEQ__
-
 cmd=$(get_normal_cmd "$seq_file")
 nvim -c "execute \"normal ${cmd}\"" 1>/dev/null 2>/dev/null
 grep -q "^\" NERDTree" "$out_file"
--- tests/test_nvim_ultisnips.sh
+++ tests/test_nvim_ultisnips.sh
@@ -9,11 +9,10 @@ source $script_dir/funcs.sh
 seq_file=$(mktemp /tmp/tmp.XXXXXXXXXX.txt)
 sh_file=$(mktemp /tmp/tmp.XXXXXXXXXX.sh)
 
-# ファイルタイプ sh で #! 入力後にタブで補完される
+# ファイルタイプ sh で #! 入力後にタブキーで補完される
 cat <<__SEQ__ | tee $seq_file
 i#!\<Tab>\<Tab>\<Esc>:wq!
 __SEQ__
-
 cmd=$(get_normal_cmd "$seq_file")
 nvim -c "execute \"normal ${cmd}\"" "$sh_file" 1>/dev/null 2>/dev/null
 grep -Fq "#!/usr/bin/env bash" "$sh_file"
