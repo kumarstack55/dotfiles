f6a65caa876486479bb180c9bbe1e73b54b47887
kumarstack55 <kumarstack55@gmail.com>
2020-04-12 05:22:03 +0000

    実行対象は実行権限があるファイルのみにした
    環境によって動かないテストが見つかったため、
    そのようなファイルの実行権限を外すことで、
    CICD時のテスト対象から外す

--- tests/run.sh
+++ tests/run.sh
@@ -4,7 +4,7 @@ set -eu
 
 script_path=$(readlink -f $0)
 script_dir=$(dirname $script_path)
-for test_path in $(find "$script_dir" -type f -name "test_*.sh"); do
+for test_path in $(find "$script_dir" -type f -executable -name "test_*.sh"); do
   echo "test_path: $test_path"
   $test_path
 done
