0a2289bfd7429db710a9c83a4cac14fd8e6cee62
kumarstack55 <kumarstack55@gmail.com>
2020-04-12 00:00:59 +0000

    test_nvim_apprentice.sh で vim script で確認するようにした

--- tests/test_nvim_apprentice.sh
+++ tests/test_nvim_apprentice.sh
@@ -6,16 +6,15 @@ script_path=$(readlink -f $0)
 script_dir=$(dirname $script_path)
 source $script_dir/funcs.sh
 
-seq_file=$(mktemp /tmp/tmp.XXXXXXXXXX.txt)
+vim_script_file=$(mktemp /tmp/tmp.XXXXXXXXXX.txt)
 out_file=$(mktemp /tmp/tmp.XXXXXXXXXX.txt)
-tmp_file=$(mktemp /tmp/tmp.XXXXXXXXXX.txt)
 
-# テキストファイルを編集するとき、Goyoコマンドが定義されている
-cat <<__SEQ__ | tee $seq_file
-:try | colorscheme apprentice | put ='ok' | endtry
+# colorsheme一覧に apprentice が存在する
+cat <<__VIM__ | tee "$vim_script_file"
+:let c = getcompletion('', 'color')
+:put =c
 :w! $out_file
 :qa!
-__SEQ__
-cmd=$(get_normal_cmd "$seq_file")
-nvim -c "execute \"normal ${cmd}\"" $tmp_file 1>/dev/null 2>/dev/null
-grep -q "ok" "$out_file"
+__VIM__
+nvim -s "$vim_script_file" 1>/dev/null 2>/dev/null
+grep -qw "apprentice" "$out_file"
