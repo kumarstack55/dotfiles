ce678c082e700fa033e88eae6f062c969f10218e
kumarstack55 <kumarstack55@gmail.com>
2020-04-12 05:24:15 +0000

    UltiSnipsのジャンプをTabからC-j,C-kに変更した

--- tests/test_nvim_ultisnips.sh
+++ tests/test_nvim_ultisnips.sh
@@ -11,7 +11,7 @@ sh_file=$(mktemp /tmp/tmp.XXXXXXXXXX.sh)
 
 # ファイルタイプ sh で #! 入力後にタブキーで補完される
 cat <<__SEQ__ | tee $seq_file
-i#!\<Tab>\<Tab>\<Esc>:wq!
+i#!\<Tab>\<c-j>\<Esc>:wq!
 __SEQ__
 cmd=$(get_normal_cmd "$seq_file")
 nvim -c "execute \"normal ${cmd}\"" "$sh_file" 1>/dev/null 2>/dev/null
