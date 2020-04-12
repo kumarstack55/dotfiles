45b1f03fcac8836066a6a5cc63ea19995547948f
kumarstack55 <kumarstack55@gmail.com>
2020-04-12 05:23:09 +0000

    一意に定まるであろうファイル名に変更した

--- tests/test_nvim_ctrlp.sh
+++ tests/test_nvim_ctrlp.sh
@@ -11,10 +11,10 @@ out_file=$(mktemp /tmp/tmp.XXXXXXXXXX.txt)
 
 # ctrl-p でファイル名を選び、ファイルを編集し、終了する
 cat <<__SEQ__ | tee $seq_file
-\<c-p>LICENSE\<CR>
+\<c-p>tests/run.sh\<CR>
 :w! ${out_file}
 :qa!
 __SEQ__
 cmd=$(get_normal_cmd "$seq_file")
 nvim -c "execute \"normal ${cmd}\"" 1>/dev/null 2>/dev/null
-grep -q "MIT License" "$out_file"
+grep -q "find" "$out_file"
