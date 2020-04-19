a1255b7a55d4b8590ceaabdb32c168203d8deb1f
kumarstack55 <kumarstack55@gmail.com>
2020-04-19 08:57:29 +0000

    inventory_to_sh.py を加えた

---
+++ bin/inventory_to_sh.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import json
+import argparse
+
+
+def print_inventory_item(inventory_item):
+    parts = list()
+    for k, v in inventory_item.items():
+        assert '"' not in v
+        parts.append('item_%s="%s"' % (k, v))
+    print(' '.join(parts))
+
+
+def print_inventory(data):
+    for item in data.get('inventory'):
+        print_inventory_item(item)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('json', nargs=1)
+    args = parser.parse_args()
+
+    with open(args.json[0]) as f:
+        data = json.load(f)
+        print_inventory(data)
