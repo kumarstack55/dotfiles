6d2aba7f99a39180707aeeccf5d1cce50a98e35a
kumarstack55 <kumarstack55@gmail.com>
2021-01-17 13:02:46 +0000

    PowerShellの補完をbashのようにする

--- src/dot.powershellrc.ps1
+++ src/dot.powershellrc.ps1
@@ -1,5 +1,10 @@
 ﻿# このファイルは $PROFILE から呼ばれる
+
+# プロンプト表示はディレクトリのみとする
 Function Prompt {
     #"PS " + $(Get-Location) + "> "
     "PS " + $(Split-Path (Get-Location) -Leaf) + "> "
 }
+
+# タブキーでの補完は、bashのようにファイルの途中までにする
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
