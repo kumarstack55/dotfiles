feb2050d358b56de30fae1498b3366c4fba0e286
kumarstack55 <kumarstack55@gmail.com>
2021-01-17 13:07:16 +0000

    testsを少し加えた

--- tests/run.sh
+++ tests/run.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
-set -eu
+set -u
 
-script_path=$(readlink -f $0)
-script_dir=$(dirname $script_path)
-source "$script_dir/funcs.sh"
+echo_err() {
+  echo "$1" 1>&2
+}
 
-txt_file=$(my_mktemp -h find)
+err_exit() {
+  echo_err "$1"
+  exit 1
+}
 
-find "$script_dir" -type f -executable -name "test_*.sh" \
-  >"$txt_file"
+ensure() {
+  if ! "$@"; then err_exit "command failed: $*"; fi
+}
 
-for test_path in $(cat "$txt_file"); do
-  echo "test_path: $test_path"
-  timeout 10s $test_path
-done
+# shellcheck disable=SC2164
+cd "$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)/.."
+
+ensure nvim '+Vader! tests/vader/*.vader'
+ensure bats tests/bats
+
+#txt_file=$(my_mktemp -h find)
+#
+#find "$script_dir" -type f -executable -name "test_*.sh" \
+#  >"$txt_file"
+#
+#for test_path in $(cat "$txt_file"); do
+#  echo "test_path: $test_path"
+#  timeout 10s $test_path
+#done
 
 echo "ok"
