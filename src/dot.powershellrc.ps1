d59f3b07dce219c97ffe9af26b3f2b967bd69072
kumarstack55 <kumarstack55@gmail.com>
2021-04-15 23:34:26 +0900

    curl のエイリアスを消した

--- src/dot.powershellrc.ps1
+++ src/dot.powershellrc.ps1
@@ -8,3 +8,6 @@ Function Prompt {
 
 # タブキーでの補完は、bashのようにファイルの途中までにする
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
+
+# curl を実行するときは curl を使いたいのであって Web-Request を使いたくない
+Remove-Item Alias:curl
