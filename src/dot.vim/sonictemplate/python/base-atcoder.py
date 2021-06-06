6cd2f58cd06f77188d1a360e4587ba0152480ec2
kumarstack55 <kumarstack55@gmail.com>
2021-06-06 22:21:41 +0900

    Update

--- src/dot.vim/sonictemplate/python/base-atcoder.py
+++ src/dot.vim/sonictemplate/python/base-atcoder.py
@@ -1,8 +1,11 @@
-import math, itertools, sys
+import sys, math, itertools, collections  # noqa: E401,E402
+sys.setrecursionlimit(100000)
 inf = 10**20
-def input_li(): return list(map(int, input().split()))
-def input_l(): return list(input().split())
-def input_i(): return int(input())
+
+
+def ili(): return list(map(int, input().split()))
+def il(): return list(input().split())
+def ii(): return int(input())
 
 
 {{_cursor_}}
