bc9a6961a9cf91e353589428a0db2bcff6edcb69
kumarstack55 <kumarstack55@gmail.com>
2021-09-27 22:33:09 +0900

    テンプレートを更新した

--- src/dot.vim/sonictemplate/python/base-atcoder.py
+++ src/dot.vim/sonictemplate/python/base-atcoder.py
@@ -1,4 +1,4 @@
-import sys
+# import sys
 # from math import ceil
 # from math import factorial
 # from math import log2
@@ -10,8 +10,22 @@ import sys
 # import queue
 
 
-sys.setrecursionlimit(100000)
-inf = 10**20
+# sys.setrecursionlimit(100000)
+# INF = 10**20
+# MOD = 998244353
+# MOD = 1000000007
+# def combinations_count(n: int, r: int):
+#     """ 異なるn個のものからr個を選ぶ場合の数を得る。 """
+#     return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
+# def sum_of_arithmetic_progressions1(a: int, d: int, n: int):
+#     """ 初項a, 公差d, 項数n の踏査数列の初項から第n項までの和を得る。 """
+#     return n * (2 * a + (n - 1) * d) // 2
+# def sum_of_arithmetic_progressions2(a: int, n: int, la: int):
+#     """ 初項a, 項数n, 末項la の踏査数列の初項から第n項までの和を得る。 """
+#     return n * (a + la) // 2
+# def sum_of_geometric_progression(a: int, r: int, n: int):
+#     """ 初項a, 公比r, 項数n の等比数列の和を得る。 """
+#     return a * (1 - r ** n) // (1 - r)
 
 
 def ili(): return list(map(int, input().split()))
@@ -20,3 +34,8 @@ def ii(): return int(input())
 
 
 {{_cursor_}}
+# N = ii()
+# N, M = ili()
+# A = ili()
+ans = -1
+print(ans)
