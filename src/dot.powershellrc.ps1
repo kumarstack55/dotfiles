2a647311118dc176ec38c822e008848d8e30cf64
kumarstack55 <kumarstack55@gmail.com>
2021-11-07 11:07:00 +0900

    プロファイルを PowerShell 7 に対応した

--- src/dot.powershellrc.ps1
+++ src/dot.powershellrc.ps1
@@ -1,13 +1,42 @@
-﻿# このファイルは $PROFILE から呼ばれる
+﻿# このファイルは $PROFILE から呼ばれる。
 
-# プロンプト表示はディレクトリのみとする
-Function Prompt {
-    #"PS " + $(Get-Location) + "> "
+# 既定では、プロンプト表示はカレントディレクトリの完全なパスを含む。
+# プロンプト表示はカレントディレクトリの名前とする。
+function Prompt {
     "PS " + $(Split-Path (Get-Location) -Leaf) + "> "
 }
 
-# タブキーでの補完は、bashのようにファイルの途中までにする
+function Get-MyPSVersionMajor {
+    <#
+        .SYNOPSIS
+        PowerShell のメジャーバージョンを得る。
+    #>
+    $PSVersionTable.PSVersion.Major
+}
+
+function Test-MyPSVersionMajorEq7 {
+    <#
+        .SYNOPSIS
+        PowerShell のバージョンが 7 か判定する。
+    #>
+    Get-MyPSVersionMajor -eq 7
+}
+
+function Test-MyPSVersionMajorEq5 {
+    <#
+        .SYNOPSIS
+        PowerShell のバージョンが 5 か判定する。
+    #>
+    Get-MyPSVersionMajor -eq 5
+}
+
+# 既定では、タブキーでの補完は、完全なコマンドを出力する。
+# タブキーでの補完は、bashのようにファイルの途中までにする。
 Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
-# curl を実行するときは curl を使いたいのであって Web-Request を使いたくない
-Remove-Item Alias:curl
+# PowerShell 5 で既定では、 curl は Web-Request のエイリアスである。
+# PowerShell 7 では、 curl はエイリアスではない。
+# 別途インストールすれば curl を実行できるようにするため、エイリアスを消す。
+Get-Alias |
+Where-Object { $_.Name -eq "curl" } |
+ForEach-Object { Remove-Item -LiteralPath "Alias:$($_.Name)" }
