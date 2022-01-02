ddb630613f0aedcbb842414cd4830aca821702ad
kumarstack55 <kumarstack55@gmail.com>
2022-01-02 10:53:04 +0000

    git clone は ssh を利用できない場合に https を試みるようにした

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,49 @@
-cd $HOME
-git clone https://github.com/kumarstack55/dotfiles.git
+#!/bin/bash
 
-cd $HOME/dotfiles/bin
-./installer.sh
+set -u
+
+usage_exit() {
+  echo "usage: $0 [options...]"
+  echo ""
+  echo "options:"
+  echo "  -h, --help: display this help and exit"
+  exit 1
+}
+
+err() {
+  echo "$1" 1>&2
+}
+
+executable() {
+  type "$1" >/dev/null 2>&1
+}
+
+main() {
+  local opt
+  while getopts -- "-:h" opt; do
+    case $opt in
+      h)  usage_exit;;
+      \?) usage_exit;;
+    esac
+  done
+  shift $((OPTIND-1))
+
+  cd $HOME
+
+  if ! executable git; then
+    err "git: command not found."
+    exit 1
+  fi
+
+  if [ ! -d dotfiles ]; then
+    git clone git@github.com:kumarstack55/dotfiles.git
+    if [[ $? -ne 0 ]]; then
+      git clone https://github.com/kumarstack55/dotfiles.git
+    fi
+  fi
+
+  cd $HOME/dotfiles/bin
+  ./installer.sh
+}
+
+main "$@"
