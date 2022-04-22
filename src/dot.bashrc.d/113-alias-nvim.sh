9a4c39f8618a68b4c682d161c3df675569499081
kumarstack55 <kumarstack55@gmail.com>
2022-04-22 23:43:55 +0900

    fix

---
+++ src/dot.bashrc.d/113-alias-nvim.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if type nvim >/dev/null 2>&1; then
+  if type vim >/dev/null 2>&1; then
+    if [[ $(uname -s) =~ ^MINGW64_NT ]]; then
+      alias nvim="winpty nvim"
+    else
+      alias vim=nvim
+    fi
+  fi
+fi
--- src/dot.bashrc.d/140-env-term.sh
+++ src/dot.bashrc.d/140-env-term.sh
@@ -4,9 +4,10 @@ if [[ ! $(uname -s) =~ ^MINGW64_NT ]]; then
     "xterm") TERM=xterm-256color;;
     *) ;;
   esac
-else
-  if [[ $(source /etc/os-release; echo "$ID") == "debian" ]]; then
-    TERM=screen-256color
-    export TERM
+  if [[ -f /etc/os-release ]]; then
+    if [[ $(source /etc/os-release; echo "$ID") == "debian" ]]; then
+      TERM=screen-256color
+      export TERM
+    fi
   fi
 fi
