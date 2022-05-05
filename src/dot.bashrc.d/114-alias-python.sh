40db13c005c2f1e98fcfa9f90f64ecd816c02838
kumarstack55 <kumarstack55@gmail.com>
2022-05-05 22:51:23 +0900

    fix

--- src/dot.bashrc.d/114-alias-python.sh
+++ src/dot.bashrc.d/114-alias-python.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-if type python >/dev/null 2>&1; then
-  if [[ $(uname -s) =~ ^MINGW64_NT ]]; then
-    alias python="winpty python"
+
+# 既存 python スクリプト動作に影響するためエイリアスを設定しない。
+if false; then
+  if type python >/dev/null 2>&1; then
+    if [[ $(uname -s) =~ ^MINGW64_NT ]]; then
+      alias python="winpty python"
+    fi
   fi
 fi
