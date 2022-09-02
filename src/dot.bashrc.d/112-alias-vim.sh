107f054f8ec9dcab891e0bdd2a4bcb31ad002bd1
kumarstack55 <kumarstack55@gmail.com>
2022-09-02 17:36:20 +0900

    git-bash の場合に nvim を使う

--- src/dot.bashrc.d/112-alias-vim.sh
+++ src/dot.bashrc.d/112-alias-vim.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if type nvim >/dev/null 2>&1; then
-  if [[ ! $(uname -s) =~ ^MINGW64_NT ]]; then
+  if [[ $(uname -s) =~ ^MINGW64_NT ]]; then
+    alias vim="winpty nvim"
+  else
     alias vim=nvim
   fi
 fi
