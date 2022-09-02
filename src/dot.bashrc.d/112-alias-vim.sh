d87fe0afab1975c4542fd47c0bb3c98996f90572
kumarstack55 <kumarstack55@gmail.com>
2022-09-02 17:28:17 +0900

    不要な条件を消した

--- src/dot.bashrc.d/112-alias-vim.sh
+++ src/dot.bashrc.d/112-alias-vim.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 if type nvim >/dev/null 2>&1; then
-  if type vim >/dev/null 2>&1; then
-    if [[ ! $(uname -s) =~ ^MINGW64_NT ]]; then
-      alias vim=nvim
-    fi
+  if [[ ! $(uname -s) =~ ^MINGW64_NT ]]; then
+    alias vim=nvim
   fi
 fi
